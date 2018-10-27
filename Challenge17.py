8c845ac1b03ad442b53f064a89b809c37f935387
Jun Ru Anderson <junruanderson@Juns-MacBook-Air.local>
2018-10-27 12:52:56 -0400

    17 almost working

--- Challenge17.py
+++ Challenge17.py
@@ -2,7 +2,6 @@ import os, codecs, random
 import Challenge10 as c10
 import Challenge15 as c15
 
-
 key = os.urandom(16)
 
 
@@ -11,6 +10,7 @@ def first_function():
         strings = file.read().split("\n")
 
     s = codecs.decode(strings[random.randint(0, len(strings) - 1)].encode(encoding='ascii'), encoding='base64')
+    print(s)
 
     IV = os.urandom(16)
 
@@ -22,6 +22,39 @@ def second_function(c, IV):
     return c15.padding_valid(s)
 
 
+def aux_extract_message(encrypted_block_before_target, target):
+    XOR_bytes = bytearray(encrypted_block_before_target)
+
+    temp = [0 for i in range(16)]
+    message_so_far = [0 for i in range(16)]
+
+    for j in range(15, -1, -1):
+        for i in range(0, 256):
+            XOR_bytes[j] = i
+            if second_function(target, XOR_bytes):
+                temp[j] = i ^ (16 - j)
+                message_so_far[j] = i ^ (16-j) ^ encrypted_block_before_target[j]
+
+                for k in range(15, j-1, -1):
+                    XOR_bytes[k] = temp[k] ^ (16 - j + 1)
+                break
+
+    return bytearray(message_so_far)
+
+
+def extract_message(c, IV):
+    prev_block = IV
+
+    message = bytearray([])
+
+    for i in range(0, len(c), 16):
+        current_block = c[i : i + 16]
+        message += aux_extract_message(prev_block, current_block)
+        prev_block = current_block
+
+    return bytes(message)
+
+
 if __name__ == "__main__":
     c, IV = first_function()
-    print(second_function(c, IV))
+    print(extract_message(c, IV))

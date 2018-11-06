46a3820b3aadc06d4f9f32cef1d092d1d3ef20a7
Jun Ru Anderson <junruanderson@Juns-MacBook-Air.local>
2018-11-05 19:22:06 -0500

    Fixed 17 last block issue

--- Challenge17.py
+++ Challenge17.py
@@ -22,6 +22,31 @@ def second_function(c, IV):
     return c15.padding_valid(s)
 
 
+def handle_last_block(encrypted_block_before_target, target):
+    message_so_far = aux_extract_message(encrypted_block_before_target, target)
+
+    if message_so_far == bytearray([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]):
+        XOR_bytes = bytearray(encrypted_block_before_target)
+        temp = [0 for i in range(16)]
+        message_so_far = [0 for i in range(16)]
+        first_time = True
+        for j in range(15, -1, -1):
+            for i in range(0, 256):
+                XOR_bytes[j] = i
+                if second_function(target, XOR_bytes):
+                    if first_time:
+                        first_time = False
+                    else:
+                        temp[j] = i ^ (16 - j)
+                        message_so_far[j] = i ^ (16 - j) ^ encrypted_block_before_target[j]
+
+                        for k in range(15, j - 1, -1):
+                            XOR_bytes[k] = temp[k] ^ (16 - j + 1)
+                        break
+
+    return bytearray(message_so_far)
+
+
 def aux_extract_message(encrypted_block_before_target, target):
     XOR_bytes = bytearray(encrypted_block_before_target)
 
@@ -49,7 +74,10 @@ def extract_message(c, IV):
 
     for i in range(0, len(c), 16):
         current_block = c[i : i + 16]
-        message += aux_extract_message(prev_block, current_block)
+        if i + 16 == len(c):
+            message += handle_last_block(prev_block, current_block)
+        else:
+            message += aux_extract_message(prev_block, current_block)
         prev_block = current_block
 
     return bytes(message)

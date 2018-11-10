b6d20ac3353a426fc529009475ce46ccd93bbe1a
Jun Ru Anderson <junruanderson@Juns-MacBook-Air.local>
2018-11-09 23:42:23 -0500

    added get blocksize to 14

--- Challenge14.py
+++ Challenge14.py
@@ -12,6 +12,19 @@ def encryption_oracle(input):
     return c10.encrypt_AES_ECB(s, c12.key)
 
 
+def get_blocksize(f):
+    count = 100
+    blocksize = 1
+    while True:
+        c = f(bytes("a" * count, encoding='ascii'))
+        if c[blocksize:blocksize*2] == c[blocksize*2:blocksize*3] == c[blocksize*3:blocksize*4]:
+            return blocksize
+
+        blocksize += 1
+        if blocksize > count // 4:
+            count += 100
+
+
 def using_ECB(f, blocksize):
     i = bytes([0 for i in range(blocksize * 2 + blocksize-padding_size[0])])
     c = f(i)
@@ -19,12 +32,12 @@ def using_ECB(f, blocksize):
 
 
 def extract_message(blocksize=16):
-    zeros = encryption_oracle(bytearray([0 for i in range(0,32)]))[16:32]
+    zeros = encryption_oracle(bytearray([0 for i in range(0,blocksize*2)]))[blocksize:blocksize*2]
     known = False
-    num_random_bytes = 16
+    num_random_bytes = blocksize
     while not known:
         b = encryption_oracle(bytearray([0 for i in range(0, 32 - num_random_bytes)]))
-        if b[16:32] == zeros:
+        if b[blocksize:blocksize*2] == zeros:
             break
         num_random_bytes -= 1
 
@@ -56,4 +69,5 @@ def extract_message(blocksize=16):
 
 
 if __name__ == "__main__":
-    print(extract_message().decode(encoding='ascii'))
+    if using_ECB(encryption_oracle, 16):
+        print(extract_message(get_blocksize(encryption_oracle)).decode(encoding='ascii'))
